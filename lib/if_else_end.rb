59592bb51520482bd7109522729936c090787016
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 01:23:31 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,7 @@
 # Write your solution here
 
-run_code_inside = false
-puts "Code before if ... end"
-if run_code_inside
-  puts "code inside"
+if true
+  puts "Even!"
+else
+  puts "Odd!"
 end
-puts "Code after if... end"
